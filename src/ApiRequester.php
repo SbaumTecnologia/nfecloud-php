95732a4199fa2e196647680524038e1e43747bb8
frcgarcia <frcgarcia@gmail.com>
2018-06-26 15:10:01 -0300

    download sincrono

--- src/ApiRequester.php
+++ src/ApiRequester.php
@@ -59,8 +59,10 @@ class ApiRequester
      */
     public function response(ResponseInterface $response)
     {
-        $this->lastResponse = $response;
-        $content = $response->getBody()->getContents();        
+        $this->lastResponse = $response;        
+        $content = $response->getBody()->getContents();
+        var_dump($content);
+        exit;
         $local_data = json_decode($content); // parse as object
         if(property_exists ($local_data , "status" ) && $local_data->status=="fail"){            
             throw new \Exception("Erro da API (v1.0): " . $local_data->message);
